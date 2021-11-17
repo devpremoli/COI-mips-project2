39f05b4156abea984ad70743d0790342f2eaf2e7
Prem20560404 <prem.oli@bison.howard.edu>
2021-11-17 15:36:00 -0500

    loading the address of input to be worked upon, and then intializing $s6 to store the final decimal value after conversion.

--- project2.s
+++ project2.s
@@ -12,3 +12,10 @@ li $v0, 8	# laoding call code 8 for reading string
 la $a0, input	# loading the address of input
 li $a1, 1001	# length of input
 syscall
+
+la $t1, input	#loading the address of input into $t1 register
+
+li $s6, 0	# for storing the sum of decimal value of each valid base 27 number (2992540%11=1, so N=27 in my case)
+
+
+storing:
