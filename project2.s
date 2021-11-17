2251b1ed6d403ac05ce964135a92cdad933a039d
Prem20560404 <prem.oli@bison.howard.edu>
2021-11-17 16:41:21 -0500

    not important commit

--- project2.s
+++ project2.s
@@ -8,14 +8,14 @@ string: .space 5
 .text
 
 main:
-li $v0, 8	# laoding call code 8 for reading string
-la $a0, input	# loading the address of input
-li $a1, 1001	# length of input
+li $v0, 8						# laoding call code 8 for reading string
+la $a0, input						# loading the address of input
+li $a1, 1001						# length of input
 syscall
 
-la $t1, input	#loading the address of input into $t1 register
+la $t1, input						#loading the address of input into $t1 register
 
-li $s6, 0	# for storing the sum of decimal value of each valid base 27 number (2992540%11=1, so N=27 in my case)
+li $s6, 0						# for storing the sum of decimal value of each valid base 27 number (2992540%11=1, so N=27 in my case)
 
 li $t8, 0
 
@@ -24,24 +24,24 @@ li $t8, 0
 storing:
 lb $t0, ($t1)
 addi $t1, $t1, 1
-beq $t0, 10, exit	#branch if character is \n
-beq $t0, 0, exit	#branch if character is null
-beq $t0, 32, storing	#branch if character is space
-beq $t0, 11, storing	#branch if character is tab
+beq $t0, 10, exit					#branch if character is \n
+beq $t0, 0, exit					#branch if character is null
+beq $t0, 32, storing					#branch if character is space
+beq $t0, 11, storing					#branch if character is tab
 
-beq $t8, 1, exit	# if this instruction is reached, valid character is found
+beq $t8, 1, exit					# if this instruction is reached, valid character is found
 li $t8, 1
 
-la $s1, string		#loading the address of string data into s1
+la $s1, string						#loading the address of string data into s1
 
 lb $t0, -1($t1)
-sb $t0, 0($s1)		#storing the first character in the first byte of $s6
+sb $t0, 0($s1)						#storing the first character in the first byte of $s6
 
 lb $t0, 0($t1)
-sb $t0, 1($s1)		#storing the second character in the second byte of $s6
+sb $t0, 1($s1)						#storing the second character in the second byte of $s6
 
 lb $t0, 1($t1)
-sb $t0, 2($s1)		#storing the third character
+sb $t0, 2($s1)						#storing the third character
 
 lb $t0, 2($t1)
 sb $t0, 3($s1)		#storing the fourth character
